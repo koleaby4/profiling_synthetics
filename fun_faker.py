8faf0b6cd23e80397106b7154d9a7ce87af59b24
Nicolai Negru <negru.n@gmail.com>
2022-05-09 15:10:52 +0100

    add a reminder note which might improve Faker's performance

--- fun_faker.py
+++ fun_faker.py
@@ -1,6 +1,7 @@
 from faker import Faker
 
 fake = Faker()
+# fake = Faker(use_weighting=False) # this should speed things up
 
 def random_int(min, max):
     return fake.random_int(min=min, max=max)
